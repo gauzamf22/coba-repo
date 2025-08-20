606f8de289ecb6f77c70dde1519020158f90b10d
gauzamf22 <gauzamf22@gmail.com>
2025-08-20 11:45:35 +0700

    nyobak cpp
    perubahan update

---
+++ hello.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+// This program prints "Hello, World!" to the console
+// std :: cout adalah standard character output 
+// karena itu adalah cara compiler mengetahui akhir dari sebuah perintah.
+//  sebagai pemisah antar pernyataan, sehingga compiler bisa membedakan satu perintah dengan perintah berikutnya.
+// Tanpa tanda ;, compiler akan menghasilkan error karena tidak tahu di mana pernyataan berakhir
+
+
+int main() {
+    std::cout << "Aku suka makan ayam" << std::endl;
+    std::cout << "Aku suka KFC" << std::endl;
+    return 0;
+}
+
+// atau bisa juga seperti ini
+//int main() {
+   // std::cout << "Aku suka makan ayam" << '\n';
+    //std::cout << "Aku suka KFC\n";
+   // return 0;
+
+// std::end1 digunakan untuk mengakhiri baris dan mengeluarkan karakter newline
+// atau dengan kata lain sebagai pemisah antar kalimat
+// std::end1 digunakan untuk mengakhiri baris dan mengeluarkan karakter newline
+
+// Dalam C++, hanya boleh ada satu fungsi main() di setiap program. Jika ada lebih dari satu, program tidak akan bisa di-compile dengan bena
+
+
+// commant bisa gini jjugak 
+
+/*
+#include <iostream
+haloi wak
+*/
+
